3b3fbb66e5440e0a69d3703b3260718832aa2d40
Andrei Ivanov <andrei.i.ivanov@commandus.com>
2018-08-07 09:56:02 +0900

    1

--- tools/js/push.js
+++ tools/js/push.js
@@ -1,24 +1,42 @@
 var webPush = require('web-push');
-webPush.setGCMAPIKey('AIzaSyAyz-oVjsfRNK53XA6o9DVHN9ZNAaHOfxw');
 webPush.setVapidDetails(
   'mailto:andrei.i.ivanov@gmail.com',
   'BM9Czc7rYYOinc7x_ALzqFgPSXV497qg76W6csYRtCFzjaFHGyuzP2a08l1vykEV1lgq6P83BOhB9xp-H5wCr1A',
   '_93Jy3cT0SRuUA1B9-D8X_zfszukGUMjIcO5y44rqCk'
 );
 
+/*
+// chrome
 var pushSubscription = {
   endpoint: 'https://fcm.googleapis.com/fcm/send/dAob7IITAjk:APA91bF0cuvHE-p8lbpdayCdiGJzNWNfu53Bd6O29cnpbz_8nzZyRHw-ChTo2o43KjGmmlk7T1GjwdaymttN_fjtp7x8gX9jC5yN2F2fPdbVkBp6IXV8wlKCvDyXZy8qDs1ObLlcl6Odic1Wu11oQIJLSZ8Df9cFEA',
   keys:{
-    p256dh:"BJ4Kwfg9qJxiQy1RlDX0W-gr5X2sy-K-jinKt_ZNbCLUPWiG0EZrull0SO1_KfRQfntJnFfbHiuCCve7GQuDQTE", 
-    auth:"aKEEhoc1tAYueXqBm_frBA"
+    p256dh:"BCjbINwYThS2ZAOKseAmax6ENhfEhqKmMMlUyzdZm2qQy_vA3tMRlOF9ReMAcvsefwHhqSt3WUyCjbUzQ53PSLw", 
+    auth:"Jc9t4JP9tonIwjzXty-WxQ"
+  }
+};
+*/
+
+var pushSubscription = {
+  endpoint: 'https://updates.push.services.mozilla.com/wpush/v2/gAAAAABbZ7cIJuyrIqApNuZd0AVjSSrYk5Cef5cI29-g8iRpHvFZzvqO6bI0ymUcf1tJpvg0lCIF7GxAbU7yg7EMXUh6c4MKaFPsSEsLzC7Mlb1JyIAMz5Wf0orVg15A2OD9dBCCUwbol78DdinNpwz-ExA67dH7InfiUDeYZS6QmVNXaPhzpGo',
+  keys:{
+    p256dh:"BBpYsgvCmjRZTlwQ__nWoeaLwuqxVc9Eg-GSloPxQdvVxapVybJKJMns8IMkYQUDiLBrnXp-qFugkPBq3fOncvY", 
+    auth:"4SgZbJVmKUP56tJ39wcWPw"
   }
 };
 
-var payload = 'Here is a payload!';
+var payload = JSON.stringify(
+  {
+    notification: {
+      title: 'Title',
+      body: 'Body',
+      icon: 'https://commandus.com/favicon.ico',
+      click_action: '"https://commandus.com/'
+    } 
+  }
+);
 
 var options = {
-  // gcmAPIKey: 'AIzaSyAyz-oVjsfRNK53XA6o9DVHN9ZNAaHOfxw',
-  TTL: 60
+  contentEncoding: 'aesgcm'
 };
 
 webPush.sendNotification(
