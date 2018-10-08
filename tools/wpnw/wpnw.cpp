0e444feea89e692d876f5eb19341fa22477bd3ef
Andrei Ivanov <andrei.i.ivanov@commandus.com>
2018-10-08 13:49:07 +0900

    checkin fixed

--- tools/wpnw/wpnw.cpp
+++ tools/wpnw/wpnw.cpp
@@ -134,6 +134,7 @@ void onLog
 int main(int argc, char **argv) 
 {
 	struct arg_str *a_file_name = arg_str0("c", "config", "<file>", "Configuration file. Default ~/" DEF_FILE_NAME);
+	struct arg_lit *a_info = arg_lit0("i", "info", "Print VAPID keys");
 	struct arg_str *a_provider = arg_str0("p", "provider", "chrome|firefox", "Re-init web push provider. Default chrome.");
 
 	struct arg_str *a_registrationid = arg_str0("r", "registration", "<id>", "Recipient registration id");
@@ -145,20 +146,21 @@ int main(int argc, char **argv)
 	struct arg_str *a_icon = arg_str0("i", "icon", "<URL>", "http[s]:// icon address.");
 	struct arg_str *a_link = arg_str0("l", "link", "<URL>", "http[s]:// action address.");
 
-	
 	struct arg_str *a_contact = arg_str0("f", "from", "<URL>", "Sender's email e.g. mailto:alice@acme.com or https[s] link");
+	struct arg_str *a_force_publickey = arg_str0("K", "public-key", "<base64>", "Override VAPID public key");
+	struct arg_str *a_force_privatekey = arg_str0("k", "private-key", "<base64>", "Override VAPID private key");
 
- 	struct arg_lit *a_aesgcm = arg_lit0("1", "aesgcm", "Force AESGCM. Default AES128GCM");
+	struct arg_lit *a_aesgcm = arg_lit0("1", "aesgcm", "Force AESGCM. Default AES128GCM");
 	struct arg_lit *a_verbosity = arg_litn("v", "verbose", 0, 4, "0- quiet (default), 1- errors, 2- warnings, 3- debug, 4- debug libs");
 	struct arg_lit *a_help = arg_lit0("h", "help", "Show this help");
 	struct arg_end *a_end = arg_end(20);
 
 	void* argtable[] = { 
-		a_file_name, a_provider,
+		a_file_name, a_info, a_provider,
 		a_registrationid, a_p256dh,
 		a_auth, 
 		a_subject, a_body, a_icon, a_link,
-		a_contact,
+		a_contact, a_force_publickey, a_force_privatekey,
 		a_aesgcm,
 		a_verbosity,
 		a_help, a_end 
@@ -179,6 +181,7 @@ int main(int argc, char **argv)
 	else
 		filename = getDefaultConfigFileName(DEF_FILE_NAME);
 	int verbosity = a_verbosity->count;
+	bool printOnly = a_info->count > 0;
 	std::string registrationid = *a_registrationid->sval;
 	std::string p256dh = *a_p256dh->sval;
 	std::string auth = *a_auth->sval;
@@ -187,6 +190,9 @@ int main(int argc, char **argv)
 	std::string link;
 	std::string body;
 	
+	std::string force_privatekey = *a_force_privatekey->sval;
+	std::string force_publickey = *a_force_publickey->sval; 
+		
 	if (a_subject->count)
 	{
 		subject = *a_subject->sval;
@@ -222,27 +228,33 @@ int main(int argc, char **argv)
 	{
 		link = "";
 	}
+	
+	bool force_vapid = (a_force_publickey->count > 0) && (a_force_privatekey->count > 0);
 
 	std::string contact = *a_contact->sval;
 	std::string cmdFileName = "curl.out";
 	bool aesgcm = a_aesgcm->count > 0;
-	if (registrationid.empty()) {
-		nerrors++;
-		std::cerr << "Recipient registration id missed." << std::endl;
+
+	if (!printOnly)
+	{
+		if (registrationid.empty()) {
+			nerrors++;
+			std::cerr << "Recipient registration id missed." << std::endl;
+		}
+		if (p256dh.empty()) {
+			nerrors++;
+			std::cerr << "Recipient public key missed." << std::endl;
+		}
+		if (auth.empty()) {
+			nerrors++;
+			std::cerr << "Recipient auth missed." << std::endl;
+		}
+		if (body.empty()) {
+			nerrors++;
+			std::cerr << "Message text missed." << std::endl;
+		}
 	}
-	if (p256dh.empty()) {
-		nerrors++;
-		std::cerr << "Recipient public key missed." << std::endl;
-	}
-	if (auth.empty()) {
-		nerrors++;
-		std::cerr << "Recipient auth missed." << std::endl;
-	}
-	if (body.empty()) {
-		nerrors++;
-		std::cerr << "Message text missed." << std::endl;
-	}
-	
+
 	enum VAPID_PROVIDER provider = PROVIDER_CHROME;
 	bool isNew = a_provider->count;
 	if (isNew) {
@@ -358,6 +370,12 @@ int main(int argc, char **argv)
 	char endpoint[256];
 	endpointC(endpoint, sizeof(endpoint), registrationid.c_str(), 1, (int) provider);	///< 0- Chrome, 1- Firefox
 
+	if (force_vapid) 
+	{
+		strncpy(privateKeyC, force_privatekey.c_str(), sizeof(privateKeyC));
+		strncpy(publicKeyC, force_publickey.c_str(), sizeof(publicKeyC));
+	}
+
 	std::cout << endpoint << std::endl;	
 	std::cout << jsonConfig(
 		provider,
@@ -370,6 +388,9 @@ int main(int argc, char **argv)
 		appId
 	) << std::endl;
 
+	if (printOnly)
+		return 0;
+	
 	time_t t = time(NULL) + 86400 - 60;
 	if (verbosity > 1)
 	{
