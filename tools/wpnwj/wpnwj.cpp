cf4b78b031413a3d9b06879599d67f420bfa4c04
andrei <andrei.i.ivanov@gmail.com>
2018-10-27 15:19:43 +0900

    autogenerated

--- tools/wpnwj/wpnwj.cpp
+++ tools/wpnwj/wpnwj.cpp
@@ -34,6 +34,7 @@
 #include <fstream>
 
 #include "utilinstance.h"
+#include "utilstring.h"
 #include "utilfile.h"
 #include "sslfactory.h"
 
@@ -45,9 +46,12 @@ static const char* progname = "wpnw";
 
 int main(int argc, char **argv) 
 {
-	struct arg_str *a_from = arg_str0(NULL, NULL, "<file>", "From");
-	struct arg_str *a_to = arg_str0(NULL, NULL, "<file>", "To");
-	struct arg_str *a_notification = arg_str0(NULL, NULL, "<file>", "Notification");
+	struct arg_str *a_from = arg_str1(NULL, NULL, "<file>", "From");
+	struct arg_str *a_to = arg_str1(NULL, NULL, "<file>", "To");
+	struct arg_str *a_notification = arg_str1(NULL, NULL, "<file>", "Notification");
+
+	// from contact
+	struct arg_str *a_contact = arg_str0("f", "from", "<URL>", "Sender's email e.g. mailto:alice@acme.com or https[s] link");
 
 	struct arg_lit *a_aesgcm = arg_lit0("1", "aesgcm", "Force AESGCM. Default AES128GCM");
 	struct arg_lit *a_verbosity = arg_litn("v", "verbose", 0, 4, "0- quiet (default), 1- errors, 2- warnings, 3- debug, 4- debug libs");
@@ -55,7 +59,8 @@ int main(int argc, char **argv)
 	struct arg_end *a_end = arg_end(20);
 
 	void* argtable[] = { 
-		a_from, a_to, a_notification, 
+		a_from, a_to, a_notification,
+		a_contact,
 		a_aesgcm,
 		a_verbosity,
 		a_help, a_end 
@@ -71,79 +76,116 @@ int main(int argc, char **argv)
 	int nerrors = arg_parse(argc, argv, argtable);
 
 	int verbosity = a_verbosity->count;
-	std::string registrationid = *a_registrationid->sval;
-	std::string p256dh = *a_p256dh->sval;
-	std::string auth = *a_auth->sval;
-	std::string subject; 
-	std::string icon;
-	std::string link;
+	
+	// Load config files and notification JSON
+	std::string from = file2string(*a_from->sval);
+	std::string to = file2string(*a_to->sval);
+	std::string notification = file2string(*a_notification->sval);;
+	
+	// Check is file empty or not
+	if (from.empty()) {
+		std::cerr << "From configuration file empty." << std::endl;
+		nerrors++;
+	}
+	if (to.empty()) {
+		std::cerr << "To configuration file empty." << std::endl;
+		nerrors++;
+	}
+	if (notification.empty()) {
+		std::cerr << "Notification file empty." << std::endl;
+		nerrors++;
+	}
+
+	enum VAPID_PROVIDER from_provider;
+	std::string from_registrationId;
+	std::string from_privateKey;
+	std::string from_publicKey;
+	std::string from_authSecret;
+	uint64_t from_androidId;
+	uint64_t from_securityToken;
+	std::string from_appId;
+
+	// validate JSON
+	int r = parseConfig
+	(
+		from,
+		from_provider,
+		from_registrationId,
+		from_privateKey,
+		from_publicKey,
+		from_authSecret,
+		from_androidId,
+		from_securityToken,
+		from_appId
+	);
+	if (r != 0) {
+		std::cerr << "From configuration file is invalid." << std::endl;
+		nerrors++;
+	}
+
+	enum VAPID_PROVIDER to_provider;
+	std::string to_registrationId;
+	std::string to_privateKey;
+	std::string to_publicKey;
+	std::string to_authSecret;
+	uint64_t to_androidId;
+	uint64_t to_securityToken;
+	std::string to_appId;
+
+	r = parseConfig
+	(
+		to,
+		to_provider,
+		to_registrationId,
+		to_privateKey,
+		to_publicKey,
+		to_authSecret,
+		to_androidId,
+		to_securityToken,
+		to_appId
+	);
+	if (r != 0) {
+		std::cerr << "To configuration file is invalid." << std::endl;
+		nerrors++;
+	}
+	
+	// validate notification JSON format
+	std::string nto;
+	std::string title;
 	std::string body;
-	
-	std::string force_privatekey = *a_force_privatekey->sval;
-	std::string force_publickey = *a_force_publickey->sval; 
-		
-	if (a_subject->count)
-	{
-		subject = *a_subject->sval;
-	}
-	else
-	{
-		subject = "";
+	std::string icon; 
+	std::string click_action;
+
+	r = parseNotificationJson(
+		notification,
+		nto,
+		title,
+		body,
+		icon, 
+		click_action
+	);
+
+	if (r != 0) {
+		std::cerr << "Notification file is invalid." << std::endl;
+		nerrors++;
 	}
 
-	if (a_body->count)
-	{
-		body = *a_body->sval;
-	}
-	else
-	{
-		body = "";
-	}
-
-	if (a_icon->count)
-	{
-		icon = *a_icon->sval;
-	}
-	else
-	{
-		icon = "";
-	}
-
-	if (a_link->count)
-	{
-		link = *a_link->sval;
-	} 
-	else 
-	{
-		link = "";
-	}
-	
 	std::string contact = *a_contact->sval;
-	std::string cmdFileName = "curl.out";
-	bool aesgcm = a_aesgcm->count > 0;
-
-	if (registrationid.empty()) {
+	bool aesgcm = a_aesgcm->count > 0;	
+	
+	if (to_registrationId.empty()) {
 		nerrors++;
 		std::cerr << "Recipient registration id missed." << std::endl;
 	}
-	if (p256dh.empty()) {
+	if (to_publicKey.empty()) {
 		nerrors++;
 		std::cerr << "Recipient public key missed." << std::endl;
 	}
-	if (auth.empty()) {
+	if (to_authSecret.empty()) {
 		nerrors++;
 		std::cerr << "Recipient auth missed." << std::endl;
 	}
-	if (body.empty()) {
-		nerrors++;
-		std::cerr << "Message text missed." << std::endl;
-	}
-
-	enum VAPID_PROVIDER provider = PROVIDER_CHROME;
-	if ("firefox" == std::string(*a_provider->sval)) {
-		provider = PROVIDER_FIREFOX;
-	}
-
+	
 	// special case: '--help' takes precedence over error reporting
 	if ((a_help->count) || nerrors)
 	{
@@ -166,53 +208,32 @@ int main(int argc, char **argv)
 	curl_global_init(CURL_GLOBAL_ALL);
 	OpenSSL_add_all_algorithms();
 
-	int r  = 0;
-
 	// write
 	char retval[4096];
 	char endpoint[256];
-	endpointC(endpoint, sizeof(endpoint), registrationid.c_str(), 1, (int) provider);	///< 0- Chrome, 1- Firefox
+	endpointC(endpoint, sizeof(endpoint), to_registrationId.c_str(), 1, (int) to_provider);	///< 0- Chrome, 1- Firefox
 
-	strncpy(privateKeyC, force_privatekey.c_str(), sizeof(privateKeyC));
-	strncpy(publicKeyC, force_publickey.c_str(), sizeof(publicKeyC));
+	strncpy(privateKeyC, from_privateKey.c_str(), sizeof(privateKeyC));
+	strncpy(publicKeyC, from_publicKey.c_str(), sizeof(publicKeyC));
 
 	if (verbosity > 0)
 	{
 		std::cerr
 			<< "endpoint: " << endpoint << std::endl
-			<< "provider: " << (provider == PROVIDER_FIREFOX ? "firefox" : "chrome") << std::endl
+			<< "provider: " << (to_provider == PROVIDER_FIREFOX ? "firefox" : "chrome") << std::endl
 			<< "privateKey: " << privateKeyC << std::endl
 			<< "publicKey: " << publicKeyC << std::endl;
 	}
-	
-	std::string msg  = mkNotificationJson(registrationid, subject, body, icon, link);
+
+	std::string msg  = mkNotificationJson(nto, title, body, icon, click_action);
 	time_t t = time(NULL) + 86400 - 60;
-	if (verbosity > 1)
-	{
-		r = webpushVapidCmdC(
-			retval, sizeof(retval),
-			publicKeyC,
-			privateKeyC,
-			cmdFileName.c_str(),
-			endpoint,
-			p256dh.c_str(),
-			auth.c_str(),
-			msg.c_str(),
-			contact.c_str(),
-			aesgcm ? AESGCM : AES128GCM,
-			t
-		);
-		if (r > 0)
-			std::cerr << "curl: " << std::endl << retval << std::endl;
-	}
-	
 	r = webpushVapidC(
 		retval, sizeof(retval),
 		publicKeyC,
 		privateKeyC,
 		endpoint,
-		p256dh.c_str(),
-		auth.c_str(),
+		to_publicKey.c_str(),
+		to_authSecret.c_str(),
 		msg.c_str(),
 		contact.c_str(),
 		aesgcm ? AESGCM : AES128GCM,
