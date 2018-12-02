32a06f1db346ba44568c6645e2969be0121686e2
andrei <andrei.i.ivanov@gmail.com>
2018-12-02 16:07:42 +0900

    Android build static libs

--- config.h
+++ config.h
@@ -19,6 +19,9 @@
 /* Define to 1 if you have the `crypto' library (-lcrypto). */
 #define HAVE_LIBCRYPTO 1
 
+/* Define to 1 if you have the `curl' library (-lcurl). */
+/* #undef HAVE_LIBCURL */
+
 /* Define to 1 if you have the <limits.h> header file. */
 #define HAVE_LIMITS_H 1
 
@@ -30,9 +33,6 @@
 
 /* Define to 1 if you have the `memset' function. */
 #define HAVE_MEMSET 1
-
-/* Define to 1 if you have the <openssl/ssl.h> header file. */
-#define HAVE_OPENSSL_SSL_H 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
